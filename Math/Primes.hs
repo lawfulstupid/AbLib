97aca8dae8b32f2c5dc078ddca182de0eb913012
Alex Bradley <a.bradley.dm@gmail.com>
2019-02-20 18:02:13 +0000

    cleanup

--- Math/Primes.hs
+++ Math/Primes.hs
@@ -3,56 +3,49 @@
 module Math.Primes where
 
 import Data.Ratio
-import Utilities
+import AbLib.Data.List (expSearch, expSearchWeak)
 
 -- sieved by eratosthenes himself
 primes :: [Integer]
-primes = aux [2..] where
-   aux :: [Integer] -> [Integer]
-   aux (p:ps) = p : (aux $ filter (\n -> mod n p /= 0) ps)
+primes = sieve [2..] where
+   sieve :: [Integer] -> [Integer]
+   sieve (p:xs) = p : sieve [ x | x <- xs, mod x p /= 0 ]
 
+-- Checks if a number is prime
 isPrime :: Integer -> Bool
 isPrime n = expSearch n primes
-
-divisors :: Integer -> [Integer]
-divisors n = let max = (floor . sqrt . fromInteger) n
-             in filter (\p -> mod n p == 0) $ primesunder max
    
--- factorise :: Integer -> [Integer]
--- factorise n = aux n (divisors n) where
-   -- aux :: Integer -> [Integer] -> [Integer]
-   -- aux n [] = [n]
-   -- aux 1 _ = []
-   -- aux n ops = let ops' = filter (\p -> mod n p == 0) ops
-                   -- p = head ops'
-               -- in  p : aux (div n p) ops'
+-- List all the prime factors of the argument
+-- Such that `(product . factorise)` == `id`
 factorise :: Integer -> [Integer]
 factorise 1 = []
 factorise n = case divisors n of
-                 [] -> [n]
-                 ps -> head ps : factorise (div n $ head ps)
+   []    -> [n]
+   (p:_) -> p : factorise (div n p)
+   where
+   -- List the small prime divisors of the argument
+   divisors :: Integer -> [Integer]
+   divisors n = filter ((0==) . mod n) . primesUnder
+      . floor . sqrt $ fromInteger n
 
 
--- primesunder :: Integer -> [Integer]
--- primesunder n = case x of
-                   -- Just xs -> xs
-                   -- Nothing -> []
-                -- where
-   -- x = do i <- expSearchWeak n primes
-          -- return $ take (i+1) primes
+-- Lists the primes less than or equal to the argument
+primesUnder :: Integer -> [Integer]
+primesUnder n = take k primes
+   where k = maybe 0 (+1) (expSearchWeak n primes)
           
-primesunder :: Integer -> [Integer]
-primesunder n = aux primes where
-   aux :: [Integer] -> [Integer]
-   aux (p:ps) = case compare p n of
-                   LT -> p : aux ps
-                   EQ -> [p]
-                   GT -> []
+-- primesUnder :: Integer -> [Integer]
+-- primesUnder n = aux primes where
+   -- aux :: [Integer] -> [Integer]
+   -- aux (p:ps) = case compare p n of
+      -- LT -> p : aux ps
+      -- EQ -> [p]
+      -- GT -> []
                    
 
 -- prime counting function: number of primes <= n
 pc :: Integer -> Int
-pc = length . primesunder
+pc = length . primesUnder
 
 
 -- multiplicative order of n modulo r
@@ -62,8 +55,8 @@ ord n a
    | gcd n a /= 1 = -1
    | otherwise = aux a where
    aux p = if mod p n == 1
-           then 1
-           else 1 + aux (a*p)
+      then 1
+      else 1 + aux (a*p)
 -- ord n a = aux $ map (\k -> mod (a^k) n) [1..] where
    -- aux (1:ks) = 1
    -- aux (_:ks) = 1 + aux ks
